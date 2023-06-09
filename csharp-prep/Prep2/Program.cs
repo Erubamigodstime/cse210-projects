2eae8dee8d5d92b4c0bd900663a8f8d3139ead89
Erubamigodstime <erubamigodstime43@gmail.com>
2023-06-09 21:20:01 +0100

    prep 2 created

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("============== Grade program ==============");
+        Console.Write(" Enter your Grade Percentage ");
+        string userPercentage = Console.ReadLine();
+        int percentage  = int.Parse(userPercentage);
+
+        string letter = "";        
+        if( percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"your percentage is  {percentage}");
+        Console.WriteLine("your grade is " +  letter);
+        if (percentage >= 70)
+        {
+           Console.WriteLine("Congratulations you did well and you passed your exams");
+        }
+        else
+        {
+            Console.WriteLine("you failed this Exam try again later ");
+        }
+      
+
+       
+       
+
+
+
+
+
+
+
+
+
     }
 }
